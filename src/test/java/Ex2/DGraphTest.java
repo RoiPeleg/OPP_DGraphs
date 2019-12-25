edbace418a6122ff09fde81968ab3ad5c690ab7e
RoiPeleg <37403170+RoiPeleg@users.noreply.github.com>
2019-12-25 11:54:08 +0200

    project structure updated.

---
+++ src/test/java/Ex2/DGraphTest.java
@@ -0,0 +1,22 @@
+package java.Ex2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DGraphTest {
+
+    @org.junit.jupiter.api.Test
+    void addNode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void connect() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeNode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeEdge() {
+    }
+}
